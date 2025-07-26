a8e76704d6349bddc851eb4528acd66a87ee9af2
Shrigupta23 <shriyanshigupta23@gmail.com>
2025-07-26 16:21:42 +0530

    Fix ESLint and ClerkProvider import

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,  // This skips ESLint during build
   },
 };
 
 module.exports = nextConfig;
+
