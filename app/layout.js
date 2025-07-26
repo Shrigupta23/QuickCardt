989428093499b0cb71d61816864c2a350f4842f4
Shrigupta23 <shriyanshigupta23@gmail.com>
2025-07-26 12:36:29 +0530

    Your commit message

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
       <html lang="en">
         <body className={`${outfit.className} antialiased text-gray-700`} >
           <Toaster />
@@ -20,5 +21,6 @@ export default function RootLayout({ children }) {
           </AppContextProvider>
         </body>
       </html>
+      </ClerkProvider>
   );
 }
